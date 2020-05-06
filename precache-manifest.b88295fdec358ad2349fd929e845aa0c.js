66267c5e1b63884d79f4901cdd6ec1b9657164c5
ayushnpatel <ayushnpatel@gmail.com>
2020-05-06 17:40:22 -0400

    Updates

---
+++ precache-manifest.b88295fdec358ad2349fd929e845aa0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0191707a7ece9ebeb528302edc226052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b538352245c522f65ed",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "984ed036ba0385bd9fe7",
+    "url": "/static/css/main.5ac9ed2f.chunk.css"
+  },
+  {
+    "revision": "7b538352245c522f65ed",
+    "url": "/static/js/2.55200dd5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.55200dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984ed036ba0385bd9fe7",
+    "url": "/static/js/main.d83b51d3.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  }
+]);
